e6f25d22e795d511cb456f3aa4fefd056168ec67
Trinh95 <lethimytrinhjps13105@gmail.com>
2022-03-15 16:41:14 +0100

    add alt for photo

--- src/Photo.js
+++ src/Photo.js
@@ -3,14 +3,19 @@ import "./Photo.css";
 
 export default function Photo(props) {
   if (props.photoData) {
+    console.log(props.photoData);
     return (
       <div className="Photo">
         <section className="row">
           {props.photoData.photos.map(function (photo, index) {
             return (
               <div className="col-6">
-                <a href={photo.src.original} target="_blank">
-                  <img src={photo.src.landscape} key={index}></img>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    key={index}
+                    alt={photo.alt}
+                  ></img>
                 </a>
               </div>
             );
